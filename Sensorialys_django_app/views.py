7df35223f19ff52dd6adcccfc410acf5556be4aa
Wbarillon <45207821+Wbarillon@users.noreply.github.com>
2020-11-05 14:08:33 +0100

    change git_repo env

--- Sensorialys_django_app/views.py
+++ Sensorialys_django_app/views.py
@@ -26,7 +26,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo(settings.GIT_REPO) 
+        repo = git.Repo('guiguifsk.eu.pythonanywhere.com/') 
         origin = repo.remotes.origin
 
         origin.pull()
